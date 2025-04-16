912337baabb9dc9eaeb1d66ec2f1bcea34830532
DemigodKS <mankok222@gmail.com>
2025-04-16 15:07:42 +0300

    Update task1 + 2.py

--- task1 + 2.py
+++ task1 + 2.py
@@ -39,7 +39,6 @@ print('All resources loaded in ', elapsed_time, 'seconds')
 
 
 task2
-var 1
 import asyncio
 import time
 import random
@@ -59,7 +58,6 @@ async def update_weather():
 
 
 async def player_actions():
-    task = asyncio.create_task(update_weather())
 
     for i in range(6):
         random_action = random.SystemRandom().choice(["I’d better find a trader, I’ve got so much loot to sell",
@@ -74,53 +72,19 @@ async def player_actions():
         # if random_action == "Game over":
         #     break
 
+
+
+async def main():
+    task = asyncio.create_task(update_weather())
+    await player_actions()
     await task
 
-asyncio.run(player_actions())
 
-var 2
-
-import asyncio
-import time
-import random
-import datetime
-
-async def update_weather():
-
-    for i in range(5):
-        random_weather = random.SystemRandom().choice(["No cloud", "Drizzle(мелкий дождь)",
-                                                "Foggy",
-                                                "Breeze",
-                                                "Chilly(прохладно)",
-                                                "Muggy(тепло и влажно)"])
-
-        print(f'weather: {random_weather}, {datetime.datetime.now().time()}')
-        await asyncio.sleep(3)
-
-
-async def player_actions():
-
-   task = asyncio.create_task(update_weather())
-
-   while True:
-    random_action = random.SystemRandom().choice(["I’d better find a trader, I’ve got so much loot to sell",
-                                                  "He’s camping the chest so we don’t steal it",
-                                                  "Good Game",
-                                                  "This gun is a real cheese-machine! Using it is just unfair",
-
-                                                  "Continue", "Game over"])
-
-    await asyncio.sleep(2)
-    print(f'Action: {random_action}, {datetime.datetime.now().time()}')
-    if random_action == "Game over":
-        break
-
-   await task
+asyncio.run(main())
 
 
 
 
-asyncio.run(player_actions())
 
 
-
+    
